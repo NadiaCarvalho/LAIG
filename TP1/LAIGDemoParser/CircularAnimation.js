6f9ce914ae6879ebc1016f58e967989fae557cc3
Rektosauros <mcmigas505@gmail.com>
2017-11-21 11:17:05 +0000

    circular animation class update

--- TP1/LAIGDemoParser/CircularAnimation.js
+++ TP1/LAIGDemoParser/CircularAnimation.js
@@ -2,8 +2,25 @@
 * CircularAnimation
 * @constructor
 */
-function CircularAnimation(scene){
+function CircularAnimation(scene, radius, angInit, angRot, velocity, center){
 	CGFobject.call(this,scene);
+
+	degToRad = Math.PI /180;
+
+	this.radius=radius;
+	this.velocity=velocity;
+	this.center=center
+	this.angInit=angInit*degToRad;
+	this.angRot=angRot*degToRad;
+
+	var date = new Date;
+	this.initialTime = date.getTime();
+	this.lastTime = initialTime;
+	this.timeNeeded = 0;
+
+	var w = this.velocity / this.radius;
+
+	this.timeNeeded = 2 * Math.PI * this.radius;
 
 };
 
@@ -13,5 +30,16 @@ CircularAnimation.prototype.constructor = CircularAnimation;
 
 CircularAnimation.prototype.display = function() {
 
-	
+	if((date.getTime()-this.initialTime)< this.timeNeeded*1000){
+		var deltaTime = (date.getTime()-this.initialTime)/1000;
+
+		var delta = this.angInit + w * deltaTime;
+
+		this.rotate(90, 0, 1, 0);
+		this.translate(this.radius, 0 , 0);
+		this.rotate(delta, 0, 1, 0);
+		this.translate(this.center[0], this.center[1], this.center[2]);
+
+		this.lastTime=date.getTime();
+	}
 }
